bdfbb00ca9727602c31aabf09e62118d2540dcdf
RussellChin <Russellchin.rc@gmail.com>
2018-07-16 13:00:51 -0400

    Contact

---
+++ PhoneApp/PhoneApp/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneApp
+{
+    class Contact
+    {
+        string firstName;
+        string lastName;
+        string phoneNumber;
+        string city;
+        string zipCode;
+
+            public Contact(string firstName, string lastName, string phoneNumber, string city,string zipCode)
+            {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+            this.city = city;
+            this.zipCode = zipCode;
+            }
+    }
+}
