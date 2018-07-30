d8f97adbf84ac6ade0271294e3ab2dbd00cd12a6
RussellChin <Russellchin.rc@gmail.com>
2018-07-30 12:24:28 -0400

    Web Challenge

--- PhoneApp/ContactLibrary/Directory.cs
+++ PhoneApp/ContactLibrary/Directory.cs
@@ -3,57 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PhoneApp;
 using System.Runtime.Serialization;
 
-namespace ContactLibrary
+namespace PhoneApp
 {
-    public enum State
-    {
-        NY, FL, VA, MD, SF, OH
-    }
+        public enum State
+        {
+            NY, FL, VA, MD, SF, OH
+        }
     public enum Country
     {
         FR = 0, US = 1, UK = 44, India = 91, Pakistan = 92, Australia = 61
     }
-    public class Directory
-    {
-        public List<Person> ContactList { get; set; }
-        public Directory()
-            {
-            ContactList = new List<Person>();
-            }
-        #region Add Function
-        public void Add(Person Perp)
-        {
-            ContactList.Add(Perp);
-        }
-        #endregion
-        #region Delete Function
-        public void Delete(string name)
-        {
-            var search = (from Perp in ContactList
-                                  where Perp.firstName.Equals(name)
-                                  select Perp).ToList();
-            Person searched = search[0];
-            ContactList.Remove(searched);
-        }
-        #endregion
-        #region Search Function 
-        public void Search(string name)
-        {
-            var search = (from Perp in ContactList
-                         where Perp.firstName.Equals(name)
-                         select Perp).ToList();
-            Person Searched = search[0];
-            Console.WriteLine(Searched.address.ToString() + " " + Searched.phone.ToString());
-        }
-        #endregion
-    }
-
-    #region Person Constructor
-    public class Person
+    public class Person 
     {
         public Person()
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            address = new Address();
+            phone = new Phone();
+        }
+        public Person(string firstName)
         {
             this.firstName = firstName;
             this.lastName = lastName;
@@ -65,14 +37,8 @@ namespace ContactLibrary
         public string lastName { get; set; }
         public Address address { get; set; }
         public Phone phone { get; set; }
-
-        public override string ToString()
-        {
-            return firstName + " " + lastName + " " + address.ToString() + " " + phone.ToString();
-        }
     }
-    #endregion
-    #region Address Constructor
+
     public class Address
     {
         public long Pid { get; set; }
@@ -82,14 +48,8 @@ namespace ContactLibrary
         public State State { get; set; }
         public Country Country { get; set; }
         public string zipcode { get; set; }
-
-        public override string ToString()
-        {
-            return houseNum + " " + street + " "+ city + " " + State + " " + Country;
-        }
     }
-    #endregion
-    #region Phone Constructor
+
     public class Phone
     {
         public long Pid { get; set; }
@@ -97,12 +57,5 @@ namespace ContactLibrary
         public string areaCode { get; set; }
         public string number { get; set; }
         public string ext { get; set; }
-
-        public override string ToString()
-        {
-            return countrycode + " " + number;
     }
-    }
-    #endregion
 }
-
--- WebApplication/WebApplicationTest/Scripts/JavaScript.js
+++ WebApplication/WebApplicationTest/Scripts/JavaScript.js
@@ -1,1 +1,13 @@
-﻿
+﻿function addressCheck()
+{
+    var check = document.querySelector("#checkAddress").value;          
+    if (check == 0) {
+        var address2 = document.querySelector("#address2");
+        var treeEl = document.createElement("treeEl");
+        treeEl.innerText = "Permanent Address: ";
+        var add1 = document.createElement("input");
+        add1.setAttribute("type", "text");
+        address2.appendChild(treeEl);
+        address2.appendChild(add1);
+    }
+}
